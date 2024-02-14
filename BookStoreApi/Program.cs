ad8ac68ce620ffc72d50cae54bbdc2d576627c3d
AlvinKimata <alvinkimata@gmail.com>
2024-02-14 14:19:59 +0300

    initial commit

---
+++ BookStoreApi/Program.cs
@@ -0,0 +1,31 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings(
+    builder.Configuration.GetSection("BookStoreDatabase")
+);
+builder.Services.AddSingleton<BooksService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
