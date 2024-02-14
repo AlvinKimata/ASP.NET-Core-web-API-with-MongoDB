46282ee0a9d2698c2d6a6f00217805bc3a522588
AlvinKimata <alvinkimata@gmail.com>
2024-02-14 14:51:52 +0300

    Add controller methods

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -4,9 +4,9 @@ using BookStoreApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.Configure<BookStoreDatabaseSettings(
-    builder.Configuration.GetSection("BookStoreDatabase")
-);
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+    
 builder.Services.AddSingleton<BooksService>();
 
 builder.Services.AddControllers();
